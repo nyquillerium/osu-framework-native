1cf017911dfe18faf8919925859c632f087e1921
David Zhao <nyquill@ppy.sh>
2019-01-03 17:04:43 +0900

    propjectify part 2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace osu_framework_native
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
